0e3ae6fd70081e436c1ffffb923e8fdc3d3349ce
Grzegorz Marcin Szczerba <vetomir@gmail.com>
2021-02-18 21:26:28 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Siema
+      <h1>elo</h1>
+{/*      <Route path='/front' exact component={Front}/>
+      <Route path='/' exact component={Home}/>
+      <Route path='/forms' exact component={Forms}/>
+      <Route path='/login' exact component={Login}/>
+      <Route path='/register' exact component={Register}/>
+      <Route path='/category-list' exact component={CategoryList}/>
+      <Route path='/category-single' exact component={CategorySingle}/>
+      <Route path='/category-create' exact component={CategoryCreate}/>
+      <Route path='/category-update' exact component={CategoryUpdate}/>
+      <Route path='/category-delete' exact component={CategoryDelete}/>*/}
     </div>
   );
 }
