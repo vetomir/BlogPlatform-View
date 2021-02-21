854685834db9b29aaa72d71a745762944719f97a
Grzegorz Marcin Szczerba <vetomir@gmail.com>
2021-02-21 21:03:25 +0100

    deploy

--- src/controllers/Config.js
+++ src/controllers/Config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-export const API_URL = 'http://localhost:8080/api';
-/*export const API_URL = 'https://portfolio-blog-spring.herokuapp.com/api';*/
+/*export const API_URL = 'http://localhost:8080/api';*/
+export const API_URL = 'https://portfolio-blog-spring.herokuapp.com/api';
 
 export default function AuthHeaders() {
     const token = JSON.parse(localStorage.getItem('user'));
