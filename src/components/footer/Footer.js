63b9d422b20dfcf59d52f10b73d2d7f0e5e7f7d0
Grzegorz Marcin Szczerba <vetomir@gmail.com>
2021-02-23 00:51:51 +0100

    cors config

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,6 @@
 import {Component} from "react";
+import styled from 'styled-components'
+import {colors} from "../utils/Colors";
 
 class Footer extends Component {
     constructor(props) {
@@ -10,10 +12,18 @@ class Footer extends Component {
     render() {
 
         return (
-            <>
+            <Wrapper>
                 Footer
-            </>
+            </Wrapper>
         )
     }
 }
 export default Footer;
+
+const Wrapper = styled.div`
+  width: 100%;
+  color: ${colors.indigo};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
--- src/controllers/Config.js
+++ src/controllers/Config.js
@@ -1,5 +1,5 @@
-/*export const API_URL = 'http://localhost:8080/api';*/
-export const API_URL = 'https://portfolio-blog-spring.herokuapp.com/api';
+export const API_URL = 'http://localhost:8080/api';
+/*export const API_URL = 'https://portfolio-blog-spring.herokuapp.com/api';*/
 
 export default function AuthHeaders() {
     const token = JSON.parse(localStorage.getItem('user'));
