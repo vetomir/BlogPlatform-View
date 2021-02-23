07468a27db5b3be776e78c5642fef207173e056b
Grzegorz Marcin Szczerba <vetomir@gmail.com>
2021-02-23 02:13:43 +0100

    cors config

--- src/controllers/Config.js
+++ src/controllers/Config.js
@@ -1,5 +1,5 @@
-/*export const API_URL = 'http://localhost:8080/api';*/
-export const API_URL = 'https://portfolio-blog-spring.herokuapp.com/api';
+export const API_URL = 'http://localhost:8080/api';
+/*export const API_URL = 'https://portfolio-blog-spring.herokuapp.com/api';*/
 
 export default function AuthHeaders() {
     const token = JSON.parse(localStorage.getItem('user'));
@@ -8,7 +8,7 @@ export default function AuthHeaders() {
         return {
             Authorization: 'Bearer ' + token ,
             'Content-Type': 'application/json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "https://portfolio-blog-spring.herokuapp.com",
+            "Access-Control-Allow-Origin": "*",
             Accept: "application/json"
         };
     } else {
